10af30315e0605a2e5492053ab504cf1e5e5ee17
rafliadis06-ux <rafliadis25@gmail.com>
2026-01-02 02:10:45 +0700

    mengubah file

---
+++ voting.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+    int jumlahPemilih, suaraSetuju = 0, suaraTidak= 0;
+    cout<<"Jumlah Pemilih: ";
+    cin>>jumlahPemilih;
+    vector<int> suara(jumlahPemilih);
+    for (int i=0; i<jumlahPemilih; i++) {
+        cout<<"Suara Pemilih Ke-"<<i+1<<" (1=Setuju, 0=Tidak Setuju): ";
+        cin>>suara[i];
+        if (suara[i] == 1) suaraSetuju++;
+        else suaraTidak++;
+    }
+    cout<<"\n===HASIL VOTING===\n";
+    cout<<"Setuju: "<<suaraSetuju<<"\n";
+    cout<<"Tidak Setuju: "<<suaraTidak<<"\n";
+    if (suaraSetuju > suaraTidak) cout<<"Keputusan: Disetujui\n";
+    else if (suaraTidak > suaraSetuju) cout<<"Keputusan: Tidak Disetujui\n";
+    else cout<<"Keputusan: Seri\n";
+    return 0;
+}
